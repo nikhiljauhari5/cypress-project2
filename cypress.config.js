aad0099f981ac5c8fc664a0883bbba629d4eabf7
nikhiljauhari5 <nikhiljauhari5@gmail.com>
2022-09-10 20:42:05 +0530

    updates

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  watchForFileChanges: false,
+  defaultCommandTimeout: 20000,
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+  },
+};
